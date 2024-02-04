5eb933dae5b998ebdc45eef2c6d087c47dbc39dc
[karol fraczek] <karol.fraczkowski@gmail.com>
2024-02-04 16:56:46 +0100

    further code refactor

--- tests/01-login.spec.ts
+++ tests/01-login.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to AutomationPractice', () => {
-  test('successful login with valid credentials', async ({ page }) => {
-    const url = 'http://www.automationpractice.pl/index.php?';
-    const userEmail = 'test@example.com';
-    const userPassword = 'Password123!';
-    const expectedAccountName = 'Karol test';
+  const userEmail = 'test@example.com';
+  const userPassword = 'Password123!';
 
-    await page.goto(url);
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://www.automationpractice.pl/');
     await page.getByRole('link', { name: 'Sign in' }).click();
+  });
+
+  test('successful login with valid credentials', async ({ page }) => {
+    const expectedAccountName = 'Karol test';
     await page.locator('#email').fill(userEmail);
     await page.getByLabel('Password').fill(userPassword);
     await page.getByRole('button', { name: ' Sign in' }).click();
@@ -17,13 +19,9 @@ test.describe('User login to AutomationPractice', () => {
   });
 
   test('unsuccessful login with invalid email', async ({ page }) => {
-    const url = 'http://www.automationpractice.pl/index.php?';
     const userInvalidEmail = 'testexample.com';
-    const userPassword = 'Password123';
     const expectedEmailAlert = 'Invalid email address.';
 
-    await page.goto(url);
-    await page.getByRole('link', { name: 'Sign in' }).click();
     await page.locator('#email').fill(userInvalidEmail);
     await page.getByLabel('Password').fill(userPassword);
     await page.getByRole('button', { name: ' Sign in' }).click();
@@ -32,13 +30,9 @@ test.describe('User login to AutomationPractice', () => {
   });
 
   test('unsuccessful login with invalid password', async ({ page }) => {
-    const url = 'http://www.automationpractice.pl/index.php?';
-    const userEmail = 'test@example.com';
     const userInvalidPassword = 'Password';
     const expectedPasswordAlert = 'Authentication failed.';
 
-    await page.goto(url);
-    await page.getByRole('link', { name: 'Sign in' }).click();
     await page.locator('#email').fill(userEmail);
     await page.getByLabel('Password').fill(userInvalidPassword);
     await page.getByRole('button', { name: ' Sign in' }).click();
@@ -47,13 +41,9 @@ test.describe('User login to AutomationPractice', () => {
   });
 
   test('unsuccessful login with too short password', async ({ page }) => {
-    const url = 'http://www.automationpractice.pl/index.php?';
-    const userEmail = 'test@example.com';
     const userShordPassword = 'Pass';
     const expectedPasswordAlert = 'Invalid password.';
 
-    await page.goto(url);
-    await page.getByRole('link', { name: 'Sign in' }).click();
     await page.locator('#email').fill(userEmail);
     await page.getByLabel('Password').fill(userShordPassword);
     await page.getByRole('button', { name: ' Sign in' }).click();
