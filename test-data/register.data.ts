193ec0f353b925aa12276a2e52a7a8bfecff8d9f
[karol fraczek] <karol.fraczkowski@gmail.com>
2024-02-06 19:56:20 +0100

    added faker

--- test-data/register.data.ts
+++ test-data/register.data.ts
@@ -1,5 +1,4 @@
 export const registerData = {
-  email: 'test@example123456.com',
   firstName: 'Karol',
   lastName: 'Test',
   password: 'Password123!',
--- tests/02-register.spec.ts
+++ tests/02-register.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { registerData } from '../test-data/register.data';
 import { RegisterPage } from '../pages/registerPage';
+import { faker } from '@faker-js/faker';
 
 test.describe('User register to AutomationPractice', () => {
   let registerPage: RegisterPage;
   const url = 'http://www.automationpractice.pl/';
+  const email = faker.internet.email()
 
   test.beforeEach(async ({ page }) => {
     registerPage = new RegisterPage(page);
@@ -13,11 +15,11 @@ test.describe('User register to AutomationPractice', () => {
   });
 
   test('successful register with valid credentials', async ({ page }) => {
-    await registerPage.fillRegisterEmailInput(registerData.email);
+    await registerPage.fillRegisterEmailInput(email);
     await registerPage.radioMrChecked();
     await registerPage.fillFirstName(registerData.firstName);
     await registerPage.fillLastName(registerData.lastName);
-    expect(registerPage.email).toHaveValue(registerData.email);
+    expect(registerPage.email).toHaveValue(email);
     await registerPage.fillPassword(registerData.password);
     await registerPage.pickDateOfBirth('10', '5', '1990');
     await registerPage.newsletterChecked();
