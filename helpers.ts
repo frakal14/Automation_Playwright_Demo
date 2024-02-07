7d26a735229d19081a9e23f16d4a93cad2add22d
[karol fraczek] <karol.fraczkowski@gmail.com>
2024-02-07 12:18:57 +0100

    addItem method refactor

--- helpers.ts
+++ helpers.ts
@@ -1,19 +1,16 @@
 export async function loadHomePage(page) {
-    const url = 'http://www.automationpractice.pl/';
+  const url = 'http://www.automationpractice.pl/';
 
-    await page.goto(url);
+  await page.goto(url);
 }
 
 // Faded Short Sleeve T-shirts blue variant size 2
 export async function addSingleProductToCart(page) {
-     const productUrl = 'http://www.automationpractice.pl/index.php?id_category=3&controller=category'
-    await page.goto(productUrl);
-      await page.locator('#color_2').click();
-      await page.getByLabel('Size').selectOption('2');
-      await page.getByRole('button', { name: ' Add to cart' }).click();
-      await page.page.getByRole('link', {name: 'Proceed to checkout '}).click()
+  const productUrl =
+    'http://www.automationpractice.pl/index.php?id_category=3&controller=category';
+  await page.goto(productUrl);
+  await page.locator('#color_2').click();
+  await page.getByLabel('Size').selectOption('2');
+  await page.getByRole('button', { name: ' Add to cart' }).click();
+  await page.getByRole('link', { name: 'Proceed to checkout ' }).click();
 }
-
-
-
-
